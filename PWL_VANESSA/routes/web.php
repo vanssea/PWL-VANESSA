3781c24dfd639e98c655a9d439c7a557e7f52a37
vanssea <cristinvanessa6@gmail.com>
2025-02-27 17:09:46 +0700

    Jobsheet 2_RouteParameters_/articles

--- PWL_VANESSA/routes/web.php
+++ PWL_VANESSA/routes/web.php
@@ -40,3 +40,8 @@ Route::get('/user/{name}', function ($name) {
 Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
     return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
 });
+
+
+Route::get('/articles/{id}', function ($id) {
+    return 'Halaman Artikel dengan ID ' .$id;
+});
