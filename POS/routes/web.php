3ba9fa4ee030d85085e60e75b71cb96a8e5346fe
vanssea <cristinvanessa6@gmail.com>
2025-02-27 21:45:59 +0700

    add: POS

---
+++ POS/routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PenjualanController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', [HomeController::class, 'index']);
+Route::get('/product', [ProductController::class, 'index']);
+
+Route::prefix('category')->group(callback: function (): void {
+    Route::get('/', [ProductController::class, 'index']);
+    Route::get('/food-beverage', [ProductController::class, 'foodBeverage']);
+    Route::get('/beauty-health', [ProductController::class, 'beautyHealth']);
+    Route::get('/home-care', [ProductController::class, 'homeCare']);
+    Route::get('/baby-kid', [ProductController::class, 'babyKid']);
+});
+
+Route::get('/user/{id}/name/{name}', [UserController::class, 'index']);
+Route::get('/penjualan', [PenjualanController::class, 'index']);
