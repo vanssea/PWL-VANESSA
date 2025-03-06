20462dff0d122867d6ae8bae7ced47afba4fb62d
vanssea <cristinvanessa6@gmail.com>
2025-03-06 17:43:20 +0700

    modif-update_Jobsheet 3_Praktikum 5

--- Week_3/POS/app/Http/Controllers/KategoriController.php
+++ Week_3/POS/app/Http/Controllers/KategoriController.php
@@ -9,13 +9,16 @@ class KategoriController extends Controller
 {
     public function index()
     {
-        $data = [
-            'kategori_kode' => 'SNK',
-            'kategori_nama' => 'Snack/Makanan Ringan',
-            'created_at' => now()
-        ];
+        // $data = [
+        //     'kategori_kode' => 'SNK',
+        //     'kategori_nama' => 'Snack/Makanan Ringan',
+        //     'created_at' => now()
+        // ];
 
-        DB::table('m_kategori')->insert($data);
-        return 'Insert data baru berhasil';
+        // DB::table('m_kategori')->insert($data);
+        // return 'Insert data baru berhasil';
+
+        $row = DB::table('m_kategori') -> where ('kategori_kode', 'SNK') -> update(['kategori_nama' => 'Camilan']);
+        return 'Update data berhasil. Jumlah data yang diupdate: ' .$row.' baris';
     }
 }
