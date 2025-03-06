03d0804c15b596ec3b60b2d81b68b23130d28e32
vanssea <cristinvanessa6@gmail.com>
2025-03-06 17:19:35 +0700

    Jobsheet3_Praktikum4

--- Week_3/POS/routes/web.php
+++ Week_3/POS/routes/web.php
@@ -1,10 +1,18 @@
 <?php
 
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\PenjualanController;
-use App\Http\Controllers\ProductController;
-use App\Http\Controllers\UserController;
+// use App\Http\Controllers\HomeController;
+// use App\Http\Controllers\PenjualanController;
+// use App\Http\Controllers\ProductController;
+// use App\Http\Controllers\UserController;
+use App\Http\Controllers\LevelController;
 use Illuminate\Support\Facades\Route;
+
+// Modifikasi Praktikum 4
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('level', [LevelController::class, 'index']);
 
 /*
 |--------------------------------------------------------------------------
@@ -17,16 +25,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [HomeController::class, 'index']);
-Route::get('/product', [ProductController::class, 'index']);
+//Route::get('/', [HomeController::class, 'index']);
+// Route::get('/product', [ProductController::class, 'index']);
 
-Route::prefix('category')->group(callback: function (): void {
-    Route::get('/', [ProductController::class, 'index']);
-    Route::get('/food-beverage', [ProductController::class, 'foodBeverage']);
-    Route::get('/beauty-health', [ProductController::class, 'beautyHealth']);
-    Route::get('/home-care', [ProductController::class, 'homeCare']);
-    Route::get('/baby-kid', [ProductController::class, 'babyKid']);
-});
+// Route::prefix('category')->group(callback: function (): void {
+//     Route::get('/', [ProductController::class, 'index']);
+//     Route::get('/food-beverage', [ProductController::class, 'foodBeverage']);
+//     Route::get('/beauty-health', [ProductController::class, 'beautyHealth']);
+//     Route::get('/home-care', [ProductController::class, 'homeCare']);
+//     Route::get('/baby-kid', [ProductController::class, 'babyKid']);
+// });
 
-Route::get('/user/{id}/name/{name}', [UserController::class, 'index']);
-Route::get('/penjualan', [PenjualanController::class, 'index']);
+// Route::get('/user/{id}/name/{name}', [UserController::class, 'index']);
+// Route::get('/penjualan', [PenjualanController::class, 'index']);
+
