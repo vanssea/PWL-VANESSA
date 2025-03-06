fd2e3c371c27bc48cb19c3c42e0a494c1f7d0e0c
vanssea <cristinvanessa6@gmail.com>
2025-03-06 17:26:02 +0700

    modif-hapus_Jobsheet 3_Praktikum 4

--- Week_3/POS/app/Http/Controllers/LevelController.php
+++ Week_3/POS/app/Http/Controllers/LevelController.php
@@ -12,7 +12,10 @@ class LevelController extends Controller
         // DB::insert('insert into m_level(level_kode, level_nama, created_at) values(?, ?, ?)', ['CUS', 'Pelanggan', now()]);
         // return 'Insert data baru berhasil';
 
-        $row = DB::update('update m_level set level_nama = ? where level_kode = ?', ['Customer', 'CUS']);
-        return 'Update data berhasil. Jumlah data yang diupdate: ' . $row . ' baris';
+        // $row = DB::update('update m_level set level_nama = ? where level_kode = ?', ['Customer', 'CUS']);
+        // return 'Update data berhasil. Jumlah data yang diupdate: ' . $row . ' baris';
+
+        $row = DB::delete('delete from m_level where level_kode = ?', ['CUS']);
+        return 'Delete data berhasil. Jumlah data yang dihapus: ' . $row . ' baris';
     }
 }
