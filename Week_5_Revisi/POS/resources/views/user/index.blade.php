bfa41664bd76727e3844fad3ac36ee6adbfc1b2d
vanssea <cristinvanessa6@gmail.com>
2025-03-27 17:43:43 +0700

    Jobsheet 5 - Praktikum 4

--- Week_5_Revisi/POS/resources/views/user/index.blade.php
+++ Week_5_Revisi/POS/resources/views/user/index.blade.php
@@ -15,13 +15,29 @@
         @if (session('error'))
             <div class="alert alert-danger">{{ session('error') }}</div>
         @endif
+        <div class="row">
+            <div class="col-md-12">
+                <div class="form-group row">
+                    <label class="col-1 control-label col-form-label">Filter:</label>
+                    <div class="col-3">
+                        <select class="form-control" id="level_id" name="level_id" required>
+                            <option value="">- Semua -</option>
+                            @foreach($level as $item)
+                                <option value="{{ $item->level_id }}">{{ $item->level_nama }}</option>
+                            @endforeach
+                        </select>
+                        <small class="form-text text-muted">Level Pengguna</small>
+                    </div>
+                </div>
+            </div>
+        </div>        
         <table class="table table-bordered table-striped table-hover table-sm" id="table_user">
             <thead>
                 <tr><th>ID</th><th>Username</th><th>Nama</th><th>Level Pengguna</th><th>Aksi</th></tr>
             </thead>
         </table>
     </div>
-        
+
 @endsection
 
 @push('css')
@@ -36,12 +52,15 @@
             ajax: {
                 url: "{{ url('user/list') }}",
                 "dataType": "json",
-                "type": "POST"
+                "type": "POST",
+                "data": function (d) {
+                    d.level_id = $('#level_id').val();
+                }
             },
             columns: [
                 {
                 // nomor urut dari laravel datatable addIndexColumn() 
-                data: "DT_RowIndex",
+                data: "DT_RowIndex", // nomor urut dari laravel datatable addIndexColumn()
                 className: "text-center", 
                 orderable: false, 
                 searchable: false
@@ -71,6 +90,9 @@
             }
         ]
     });
+    $('#level_id').on('change', function() {
+        dataUser.ajax.reload();
+    })
     });
     </script> 
 @endpush
