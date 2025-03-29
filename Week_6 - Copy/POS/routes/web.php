13822d3736085a6380a8684346613114fe487d84
vanssea <cristinvanessa6@gmail.com>
2025-03-29 21:43:36 +0700

    Jobsheet 6 - Praktikum 2

--- Week_6 - Copy/POS/routes/web.php
+++ Week_6 - Copy/POS/routes/web.php
@@ -21,6 +21,8 @@ Route::group(['prefix' => 'user'], function () {
     Route::get('/{id}', [UserController::class, 'show']);            // menampilkan detail user
     Route::get('/{id}/edit', [UserController::class, 'edit']);       // menampilkan halaman form edit user
     Route::put('/{id}', [UserController::class, 'update']);          // menyimpan perubahan data user
+    Route::get('/{id}/edit_ajax', [UserController::class, 'edit_ajax']);       // menampilkan halaman form edit user Ajax
+    Route::put('/{id}/update_ajax', [UserController::class, 'update_ajax']);          // menyimpan perubahan data user Ajax
     Route::delete('/{id}', [UserController::class, 'destroy']);      // menghapus data user
 });
 
