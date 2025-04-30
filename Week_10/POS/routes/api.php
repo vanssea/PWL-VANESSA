1597d1e4245a92cb99b85d5c1bf7ebc85bc961dd
vanssea <cristinvanessa6@gmail.com>
2025-04-30 19:23:01 +0700

    Jobsheet 10_Praktikum 3 - Membuat RESTful API Logout

--- Week_10/POS/routes/api.php
+++ Week_10/POS/routes/api.php
@@ -2,29 +2,20 @@
 
 use App\Http\Controllers\Api\RegisterController;
 use App\Http\Controllers\Api\LoginController;
+use App\Http\Controllers\Api\LogoutController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+Route::post('/register', RegisterController::class)->name('register');
+Route::post('/login', LoginController::class)->name('login');
 
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
- 
- Route::post('/register', App\Http\Controllers\Api\RegisterController::class)->name('register');
+// Rute dengan middleware auth:api
+Route::middleware('auth:api')->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
 
- Route::post('/register', App\Http\Controllers\Api\RegisterController::class)->name('register');
- Route::post('/login', App\Http\Controllers\Api\LoginController::class)->name('login');
- Route::middleware('auth:api')->get('/user', function (Request $request) {
-     return $request->user();
- });
+    Route::post('/logout', LogoutController::class)->name('logout');
+});
+
  
